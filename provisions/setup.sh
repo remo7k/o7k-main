e04a6cf4d8ce55021f7de773b95195029b6eaf74
rem <remy.o@outlook.com>
2022-12-23 12:39:43 +0100

    update setup

--- provisions/setup.sh
+++ provisions/setup.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 set -e
-# sudo apt-get install curl -y
 
-# 1. install openssh -y
-# sudo apt-get install openssh-server -y
-# sudo systemctl enable ssh
-# sudo systemctl start ssh
+# shellcheck disable=SC2016
+: '
+sudo apt-get install curl -y
 
-# 2. install git
-# sudo apt-get install git -y
-
-# 3. install docker + docker compose
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# 3.1 add docker user -- no sudo
-# sudo sh ./get-docker.sh
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
-# newgrp docker
-
-# 4. zibgee2mqtt
-# cd /o7k/o7k-station/images/zigbee2mqtt
-# change mosquitto ip in config
+1. install openssh -y
+sudo apt-get install openssh-server -y
+sudo systemctl enable ssh
+sudo systemctl start ssh
 
 
+---
 
-# docker compose up -d
-# cd /o7k/o7k-station/images/hass
-# docker compose up -d
-# cd /o7k/o7k-station/images/mosquitto
-# docker compose up -d
+2. install git
+sudo apt-get install git -y
 
 
+---
+3. install docker + docker compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+  3.1 add docker user -- no sudo
+sudo sh ./get-docker.sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+---
+4. zibgee2mqtt
+cd /o7k/o7k-station/images/zigbee2mqtt
+change mosquitto ip in config
+
+setup new network in docker
+$ docker network create -d bridge NAME_OF_NETWORK
+add mqtt container ip to config
+--
+5. mqtt
+cd /o7k/o7k-station/images/mosquitto
+docker compose up -d
+--
+6. setup haas
+docker compose up -d
+cd /o7k/o7k-station/images/hass
+docker compose up -d
+'
