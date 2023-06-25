59142ccd7bbb38c68e2219e4d5ba8fbc4bd16cf5
Remy Oleszczuk <remy.o@outlook.com>
2023-06-25 18:37:50 +0200

    add Dockerfile & gcloud script for creating a service account

---
+++ k8s/backup/gcloudscript.sh
@@ -0,0 +1,3 @@
+gcloud iam service-accounts create o7k-test --display-name "o7k test backup service account"
+gcloud iam service-accounts keys create credentials-o7k-test --iam-account o7k-test@o7k-master.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding o7k-master --member serviceAccount:o7k-test@o7k-master.iam.gserviceaccount.com --role roles/storage.admin
