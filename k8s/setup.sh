0076f593dbda5916f48394751c17a0cea33081f9
Remy Oleszczuk <remy.o@outlook.com>
2024-01-08 13:38:33 +0100

    1

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -3,8 +3,10 @@
 DIR1="/o7k/configs/frigate/config"
 DIR2="/o7k/configs/frigate/storage"
 DIR3="/o7k/configs/hass"
-DIR4="/o7k/configs/mosquitto"
+DIR4="/o7k/configs/mosquitto/config"
 
-mkdir -p "$DIR1" "$DIR2" "$DIR3"
+mkdir -p "$DIR1" "$DIR2" "$DIR3" "$DIR4"
 
-touch "$DIR1/config.yaml"
+cp ../frigate/config.yaml "$DIR1"
+cp ../mqtt/mosquitto.conf "$DIR4"
+
